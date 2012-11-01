ca254cad2fbdf4379542d37585712eaf4d0d1a8d
Eldar Djafarov <djkojb@gmail.com>
2012-11-01 14:36:15 +0200

    fix

---
+++ cookbooks/phantomjs/metadata.rb
@@ -0,0 +1,17 @@
+maintainer 'CustomInk'
+maintainer_email 'webops@customink.com'
+license 'Apache 2.0'
+description 'Installs/Configures phantomjs'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.0.7'
+
+recipe 'phantomjs::default', 'Install phantomjs binary'
+
+%w(amazon centos debian fedora oracle rhel scientific ubuntu).each do |os|
+  supports os
+end
+
+attribute 'version',
+  :display_name => 'Version',
+  :description => 'The Version of phantomjs to install',
+  :default => '1.7.0'
